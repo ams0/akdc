deefeb439e518625f660b2d79c6361a3e52b0610
bartr <bartr@microsoft.com>
2022-02-04 08:14:18 +0000

    updated scripts

--- flux-check.sh
+++ flux-check.sh
@@ -4,5 +4,5 @@
 
 for line in $(cat ips | sort | cut -f2);
 do
-  ssh -p 2222 -o "StrictHostKeyChecking=no" akdc@$line 'if [[ $(kubectl get pods -A) == *"kustomize-controller"* ]]; then echo "$(hostname) success"; else echo "$(hostname) failed"; fi'
+  ssh -p 2222 -o "StrictHostKeyChecking=no" -o ConnectTimeout=5 akdc@$line 'if [[ $(kubectl get pods -A) == *"kustomize-controller"* ]]; then echo "$(hostname) success"; else echo "$(hostname) failed"; fi'
 done
--- ngsa-check.sh
+++ ngsa-check.sh
@@ -4,5 +4,5 @@
 
 for line in $(cat ips | sort | cut -f2);
 do
-  ssh -p 2222 -o "StrictHostKeyChecking=no" akdc@$line 'if [[ $(kubectl get pods -A) == *"ngsa-memory"* ]]; then echo "$(hostname) found"; else echo "$(hostname) not found"; fi'
+  ssh -p 2222 -o "StrictHostKeyChecking=no" -o ConnectTimeout=5 akdc@$line 'if [[ $(kubectl get pods -A) == *"ngsa-memory"* ]]; then echo "$(hostname) found"; else echo "$(hostname) not found"; fi'
 done
