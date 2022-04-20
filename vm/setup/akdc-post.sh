b48d4a2537a5d2af06ba322fcc6707498dcdd977
bartr <bartr@microsoft.com>
2022-04-20 21:06:16 +0000

    updated vm names

--- vm/setup/akdc-post.sh
+++ vm/setup/akdc-post.sh
@@ -13,7 +13,7 @@ docker run -d --restart always --net akdc --name heartbeat -p 30082:8080 ghcr.io
 
 if [[ "$(hostname)" == *"monitor"* ]]
 then
-    docker run -d --restart always --name webv-heartbeat -p 30088:8080 ghcr.io/cse-labs/webv-red:beta -s https://central-tx-atx-512.cseretail.com https://east-ga-atl-512.cseretail.com https://west-wa-sea-512.cseretail.com -f heartbeat-benchmark.json --run-loop --prometheus --sleep 5000
+    docker run -d --restart always --name webv-heartbeat -p 30088:8080 ghcr.io/cse-labs/webv-red:beta -s https://central-tx-atx-510.cseretail.com https://east-ga-atl-510.cseretail.com https://west-wa-sea-510.cseretail.com -f heartbeat-benchmark.json --run-loop --prometheus --sleep 5000
 else
     docker run -d --restart always --net akdc --name imdb -p 30080:8080 ghcr.io/cse-labs/imdb-app
 fi
