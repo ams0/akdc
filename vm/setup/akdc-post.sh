63108ac557dffceba6f38d278f01476d5230d387
bartr <bartr@microsoft.com>
2022-04-20 01:07:30 +0000

    updated setup

--- vm/setup/akdc-post.sh
+++ vm/setup/akdc-post.sh
@@ -8,10 +8,13 @@ docker pull ghcr.io/cse-labs/webv-red:beta
 docker pull ghcr.io/cse-labs/heartbeat
 docker pull ghcr.io/cse-labs/imdb-app
 
-docker network create imdb
+docker network create akdc
 
-docker run -d --restart always --net imdb --name heartbeat -p 30082:8080 ghcr.io/cse-labs/heartbeat
-docker run -d --restart always --net imdb --name imdb      -p 30080:8080 ghcr.io/cse-labs/imdb-app
-docker run -d --restart always --net imdb --name webv      -p 30088:8080 ghcr.io/cse-labs/webv-red:beta -s http://imdb:8080 -f benchmark.json --run-loop --prometheus --sleep 100 --verbose
+if [[ "$(hostname)" != *"monitor"* ]]
+then
+    docker run -d --restart always --net akdc --name heartbeat -p 30082:8080 ghcr.io/cse-labs/heartbeat
+    docker run -d --restart always --net akdc --name imdb      -p 30080:8080 ghcr.io/cse-labs/imdb-app
+    docker run -d --restart always --net akdc --name webv      -p 30088:8080 ghcr.io/cse-labs/webv-red:beta -s http://imdb:8080 -f benchmark.json --run-loop --prometheus --sleep 100 --verbose
+fi
 
 echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-post complete" >> "/home/${AKDC_ME}/status"
