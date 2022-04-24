9f297da26d3ef048743e9938f8d691aa7031ff91
Bart Robertson <bartr@microsoft.com>
2022-04-24 20:02:21 +0000

    updated codespaces

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,5 +5,7 @@
 echo "post-start start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 
+sudo mount -a
+
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
