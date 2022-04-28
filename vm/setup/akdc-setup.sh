3b5e49557767c29dba8bfd4a7201234816df8941
bartr <bartr@microsoft.com>
2022-04-28 06:46:12 +0000

    updated setup

--- vm/setup/akdc-setup.sh
+++ vm/setup/akdc-setup.sh
@@ -24,7 +24,7 @@
 echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-setup start" >> "/home/${AKDC_ME}/status"
 
 # can't continue without akdc-install.sh
-if [ ! -f ./cli/vm/setup/akdc-install.sh ]
+if [ ! -f "$HOME"/cli/vm/setup/akdc-install.sh ]
 then
   echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-install.sh not found" >> "/home/${AKDC_ME}/status"
   echo "akdc-install.sh not found"
@@ -32,7 +32,7 @@ then
 fi
 
 # can't continue without akdc-dns.sh
-if [ ! -f ./cli/vm/setup/akdc-dns.sh ]
+if [ ! -f "$HOME"/cli/vm/setup/akdc-dns.sh ]
 then
   echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-dns.sh not found" >> "/home/${AKDC_ME}/status"
   echo "akdc-dns.sh not found"
@@ -40,7 +40,7 @@ then
 fi
 
 # can't continue without k8s-setup.sh
-if [ ! -f ./cli/vm/setup/k8s-setup.sh ]
+if [ ! -f "$HOME"/cli/vm/setup/k8s-setup.sh ]
 then
   echo "$(date +'%Y-%m-%d %H:%M:%S')  k8s-setup.sh not found" >> "/home/${AKDC_ME}/status"
   echo "k8s-setup.sh not found"
@@ -48,7 +48,7 @@ then
 fi
 
 # can't continue without flux-setup.sh
-if [ ! -f ./cli/vm/setup/flux-setup.sh ]
+if [ ! -f "$HOME"/cli/vm/setup/flux-setup.sh ]
 then
   echo "$(date +'%Y-%m-%d %H:%M:%S')  flux-setup.sh not found" >> "/home/${AKDC_ME}/status"
   echo "flux-setup.sh not found"
@@ -58,8 +58,8 @@ fi
 set -e
 
 # run setup scripts
-./cli/vm/setup/akdc-install.sh
-./cli/vm/setup/akdc-dns.sh
+"$HOME"/cli/vm/setup/akdc-install.sh
+"$HOME"/cli/vm/setup/akdc-dns.sh
 
 # don't run the rest of setup in debug mode
 if [ "$AKDC_DEBUG" = "true" ]
@@ -70,56 +70,56 @@ then
 fi
 
 # run akdc-pre-k8s.sh
-if [ -f ./cli/vm/setup/akdc-pre-k8s.sh ]
+if [ -f "$HOME/cli/vm/setup/akdc-pre-k8s.sh" ]
 then
   # run as AKDC_ME
-  ./cli/vm/setup/akdc-pre-k8s.sh
+  "$HOME"/cli/vm/setup/akdc-pre-k8s.sh
 else
   echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-pre-k8s.sh not found" >> "/home/${AKDC_ME}/status"
 fi
 
 # run k8s-setup
-./cli/vm/setup/k8s-setup.sh
+"$HOME"/cli/vm/setup/k8s-setup.sh
 
 # run akdc-pre-flux.sh
-if [ -f ./cli/vm/setup/akdc-pre-flux.sh ]
+if [ -f "$HOME"/cli/vm/setup/akdc-pre-flux.sh ]
 then
-  ./cli/vm/setup/akdc-pre-flux.sh
+  "$HOME"/cli/vm/setup/akdc-pre-flux.sh
 else
   echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-pre-flux.sh not found" >> "/home/${AKDC_ME}/status"
 fi
 
 # setup flux
-./cli/vm/setup/flux-setup.sh
+"$HOME"/cli/vm/setup/flux-setup.sh
 
 # run akdc-pre-arc.sh
-if [ -f ./cli/vm/setup/akdc-pre-arc.sh ]
+if [ -f "$HOME"/cli/vm/setup/akdc-pre-arc.sh ]
 then
-  ./cli/vm/setup/akdc-pre-arc.sh
+  "$HOME"/cli/vm/setup/akdc-pre-arc.sh
 else
   echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-pre-arc.sh not found" >> "/home/${AKDC_ME}/status"
 fi
 
 # setup azure arc
-if [ -f ./cli/vm/setup/arc-setup.sh ]
+if [ -f "$HOME"/cli/vm/setup/arc-setup.sh ]
 then
-  ./cli/vm/setup/arc-setup.sh
+  "$HOME"/cli/vm/setup/arc-setup.sh
 else
   echo "$(date +'%Y-%m-%d %H:%M:%S')  arc-setup.sh not found" >> "/home/${AKDC_ME}/status"
 fi
 
 # run akdc-private-repos.sh
-if [ -f ./cli/vm/setup/akdc-private-repos.sh ]
+if [ -f "$HOME"/cli/vm/setup/akdc-private-repos.sh ]
 then
-  ./cli/vm/setup/akdc-private-repos.sh
+  "$HOME"/cli/vm/setup/akdc-private-repos.sh
 else
   echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-private-repos.sh not found" >> "/home/${AKDC_ME}/status"
 fi
 
 # run akdc-post.sh
-if [ -f ./cli/vm/setup/akdc-post.sh ]
+if [ -f "$HOME"/cli/vm/setup/akdc-post.sh ]
 then
-  ./cli/vm/setup/akdc-post.sh
+  "$HOME"/cli/vm/setup/akdc-post.sh
 else
   echo "$(date +'%Y-%m-%d %H:%M:%S')  akdc-post.sh not found" >> "/home/${AKDC_ME}/status"
 fi
